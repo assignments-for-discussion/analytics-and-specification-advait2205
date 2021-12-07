66b7fcf1f0abbdae5f9eab2a785ad9337bad8a2d
Advait Acharya <advaitacharya22@gmail.com>
2021-12-07 17:30:33 +0530

    Updated average.js

--- average.js
+++ average.js
@@ -1,7 +1,26 @@
-
 function average(numbers) {
-  const newArray = numbers.filter( value => !Number.isNaN(value) );
-  return newArray.reduce((p, c)=> p + c, 0) / newArray.length;
+  const arrayWithoutNaN = numbers.filter( (value) => !Number.isNaN(value) );
+  if (!arrayWithoutNaN.length) return NaN;
+
+  const meanWithOutliers = arrayWithoutNaN
+      .reduce((p, c)=> p + c, 0) / arrayWithoutNaN.length;
+
+  const diffArray = arrayWithoutNaN.map((k)=>{
+    return (k - meanWithOutliers) ** 2;
+  });
+
+  const sum = diffArray.reduce((acc, curr)=> acc + curr, 0);
+
+  const dev = Math.sqrt(sum / diffArray.length);
+
+  // eslint-disable-next-line max-len
+  const arrayWithoutOutliers = arrayWithoutNaN
+      .filter((n) => n>=(meanWithOutliers-2*dev) && n<=(meanWithOutliers+2*dev),
+      );
+  const meanWithoutOutliers=arrayWithoutOutliers
+      .reduce((p, c) => p+c, 0,
+      ) / arrayWithoutOutliers.length;
+  return meanWithoutOutliers;
 }
 
 module.exports = {average};
